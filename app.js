224eb4478aa17d951267cd3d56f26df5a92402cc
GarvG <67434601+GarvG@users.noreply.github.com>
2021-11-30 23:16:44 +0530

    mark12

---
+++ app.js
@@ -0,0 +1,16 @@
+const input=document.querySelectorAll(".angle-input");
+const btn=document.querySelector("#check-button");
+const output=document.querySelector("#output");
+
+function sumangle(angle1,angle2,angle3)
+{
+    const sum=angle1+angle2+angle3;
+}
+
+
+btn.addEventListener("click",function isTriangle()
+{
+    
+    sumangle()
+
+});
